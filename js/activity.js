c9bb521c3cbb1cdc4f78c2b0b82469eef373ee78
puneetgkaur <puneet.gkaur@gmail.com>
2014-04-12 20:57:26 +0530

    commit

--- js/activity.js
+++ js/activity.js
@@ -7,11 +7,39 @@ define(function (require) {
         // Initialize the activity.
         activity.setup();
 
-        pygame_image_stream = function () {
-            activity.showObjectChooser(function (error, result) {
-                web_cam_output.src=result;
+	pygame_init_camera();
+
+	stop_camera();
+
+	while(1)
+	{
+            pygame_image_stream();
+	}
+
+        pygame_init_camera = function () {
+            activity.init_camera(function (error, result) {
+		alert("Reached here 1");
             });
         };
+
+        pygame_image_stream = function () {
+            activity.get_cam_image(function (error, result) {
+                web_cam_output.src=result;
+		//alert("Reached here 2");
+		//alert(result);
+            });
+        };
+
+        stop_camera = function () {
+            activity.stop_camera(function (error, result) {
+		alert("Reached here 3");
+            });
+        };
+
+
+/*
+
+
 	flag=0;
 	click=0;
 	click_photo = function() {
@@ -42,6 +70,7 @@ define(function (require) {
 	}
 
 
+*/
 
     });
 
